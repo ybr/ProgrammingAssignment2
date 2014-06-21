424f169aaff5b29afff761b559a91e5fc471fabc
ybr <ybr@zenexity.com>
2014-06-21 16:11:20 +0200

    Add some comments for sake of comprehension

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix emulates a cache on top of a matrix
+## cacheSolve is a able to use the emulated matrix from makeCacheMatrix
 
-## Write a short comment describing this function
-
+## Given a matrix this function stores its inverse
 makeCacheMatrix <- function(x = matrix()) {
+  # the cached inverse of the matrix
   inverse <- NULL
+  # replaces the matrix with the newMatrix
+  # and invalidates the cached inverse of the previous matrix
   set <- function(newMatrix) {
     x <<- newMatrix
     inverse <<- NULL
   }
+  # get the matrix
   get <- function() x
+  # set the inverse computed elsewhere
   setinverse <- function(computedInverse) inverse <<- computedInverse
+  # get the cached inverse (can be NULL)
   getinverse <- function() inverse
+  # functions accessible from the value returned by makeCacheMatrix
   list(
     set = set,
     get = get,
@@ -20,9 +26,11 @@ makeCacheMatrix <- function(x = matrix()) {
   )
 }
 
-
+## Given an x this function is able
+# - to retrieve a possibly cached inverse inside x
+# - to compute the inverse of the contained matrix and store it inside x
+# Returns the inverse of the matrix inside x
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
   # prerequires x is a square invertible matrix
 
   # if the cache is empty (no inverse inside x)
@@ -30,9 +38,11 @@ cacheSolve <- function(x, ...) {
     # then compute the inverse from the matrix inside x
     print("cache empty => computing inverse")
     computedInverse <- solve(x$get())
+    # set the computed inverse inside x (so that it is cached)
     x$setinverse(computedInverse)
   } else print("inverse already cached")
   # at this point we are sure x contains a cached inverse,
   # either it already existed either it has just been computed
+  # it is safe to return the cached inverse from x
   x$getinverse()
 }
