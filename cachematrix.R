edeaa495ce2049553094fdf0bbe76bf5d855311e
ybr <ybr@zenexity.com>
2014-06-21 15:55:14 +0200

    My solution to the caching the inverse of a matrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,35 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(newMatrix) {
+    x <<- newMatrix
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(computedInverse) inverse <<- computedInverse
+  getinverse <- function() inverse
+  list(
+    set = set,
+    get = get,
+    setinverse = setinverse,
+    getinverse = getinverse
+  )
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  # prerequires x is a square invertible matrix
+
+  # if the cache is empty (no inverse inside x)
+  if(is.null(x$getinverse())) {
+    # then compute the inverse from the matrix inside x
+    print("cache empty => computing inverse")
+    computedInverse <- solve(x$get())
+    x$setinverse(computedInverse)
+  } else print("inverse already cached")
+  # at this point we are sure x contains a cached inverse,
+  # either it already existed either it has just been computed
+  x$getinverse()
 }
